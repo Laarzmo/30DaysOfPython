485e22d4354c82f00dd8b82960f7ad08b4bff515
Laarzmo <lauri.liehu@iki.fi>
2024-09-11 19:48:26 +0300

    day3

---
+++ day_03/excericise.py
@@ -0,0 +1,34 @@
+import math
+age = int()
+height = float()
+comp = complex()
+
+'''
+print('The area of the triangle is:',float(input('Base:')) * float(input('Height:')) * 0.5)
+
+print('The perimater of the triangle is:',float(input('Sida a: ')) + float(input('Side b: ')) + float(input('Side c: ')))
+
+length = float(input('length: '))
+width = float(input('width: '))
+print('area: ',length * width)
+print('perimeter: ',(length + width) * 2 )
+
+radius = float(input('Radius: '))
+print('Area: ', radius ** 2 * 3.14)
+print('Circum: ', radius * 2 * 3.14)
+
+ 
+m8 = 2
+p1 = [2,2]
+p2 = [6,10]
+m9 = (p2[1]-p1[1])/(p2[0]-p1[0])
+
+print(p1,p2,m8,m9)
+print(math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2))  
+
+x = float(input('X: ')) # -3 -> y=0
+y = x ** 2 + 6 * x + 9
+print('y: ',y)'''
+
+print(len('python') != len('dragon'))
+print('on' in ('python' and 'dragon'))
