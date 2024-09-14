2e5ed40eb1dc17f12469c551d00ce5d96a950196
Laarzmo <lauri.liehu@iki.fi>
2024-09-14 11:18:04 +0300

    Day 03

--- day_03/excericise.py
+++ day_03/excericise.py
@@ -28,7 +28,28 @@ print(math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2))
 
 x = float(input('X: ')) # -3 -> y=0
 y = x ** 2 + 6 * x + 9
-print('y: ',y)'''
+print('y: ',y)
 
 print(len('python') != len('dragon'))
 print('on' in ('python' and 'dragon'))
+print('jargon' in 'I hope this course is not full of jargon.')
+print(str(float(len('python'))))
+print((float(input('Gimmi numer')) % 2) == 0)
+print(( 7 // 3 ) == int(2.7))
+print(type('10') == type(10))
+print(int(9.8) == 10)
+
+print(float(input('Hours: ')) * float(input('rate: ')))
+print(int(input('Years lived: '))*3600*365)
+'''
+
+x = 1
+print(x,1,x,x**2,x**3)
+x += 1
+print(x,1,x,x**2,x**3)
+x += 1
+print(x,1,x,x**2,x**3)
+x += 1
+print(x,1,x,x**2,x**3)
+x += 1
+print(x,1,x,x**2,x**3)
